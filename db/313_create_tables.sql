18adb32ea298d2ccec1dbe5bc6938f90d51fe808
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-10-14 13:29:53 +0000

    Test

---
+++ db/313_create_tables.sql
@@ -0,0 +1,8 @@
+--
+-- Database Schema:  cae-schema
+-- Automatically generated sql script for the service 313, created by the CAE.
+-- --------------------------------------------------------
+
+
+
+
