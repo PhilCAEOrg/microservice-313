b8da38bc0774f55cb8a76cefbca737f017acae97
PhilCAEUser <mail@rwth>
2020-10-14 13:34:05 +0000

    Test

--- db/313_create_tables.sql
+++ db/313_create_tables.sql
@@ -3,6 +3,14 @@
 -- Automatically generated sql script for the service 313, created by the CAE.
 -- --------------------------------------------------------
 
+--
+-- Table structure for table tab.
+--
+CREATE TABLE cae-schema.tab (
+  sjdf VARCHAR(255) ,
+CONSTRAINT sjdf_PK PRIMARY KEY (sjdf)
+ 
+);
 
 
 
